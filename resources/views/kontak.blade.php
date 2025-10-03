183bed453c800c1be1fa4f415628b176c2fcb955
icen6 <ihsan4523069@univpancasila.ac.id>
2025-10-03 11:34:04 +0700

    LaraPress

---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kontak - LaraPress</title>
+</head>
+<body>
+    <h1>Hubungi Kami</h1>
+    <p>Email: EmailKampus.univpancasila.ac.id</p>
+    <p>Telepon: +62 812-3456-7890</p>
+
+    <a href="/">Kembali ke Halaman Utama</a><br>
+    <a href="/tentang-kami">Lihat Halaman Tentang Kami</a>
+</body>
+</html>
